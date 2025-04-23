7e660c568a22a9bab7096b4822d109ee38a6b237
btms_wsl_notebook <smtbssa@gmail.com>
2025-04-23 19:35:02 -0300

    resolvido compara_assinatura

--- coursera/usp/coh_piah.py
+++ coursera/usp/coh_piah.py
@@ -70,10 +70,7 @@ def n_palavras_diferentes(lista_palavras):
     return len(freq)
 
 def compara_assinatura(as_a, as_b):
-    cal_ass_a = calcula_assinatura(as_a)
-    cal_ass_b = calcula_assinatura(as_b)
-
-    return sum([abs(t[0] - t[1]) for t in zip(cal_ass_a, cal_ass_b)]) / 6
+    return sum([abs(t[0] - t[1]) for t in zip(as_a, as_b)]) / 6
 
 def calcula_assinatura(texto):
     sentencas = separa_sentencas(texto)
@@ -109,4 +106,8 @@ def avalia_textos(textos, ass_cp):
 
 if __name__ == '__main__':
     ass = calcula_assinatura("Então resolveu ir brincar com a Máquina pra ser também imperador dos filhos da mandioca. Mas as três cunhas deram muitas risadas e falaram que isso de deuses era gorda mentira antiga, que não tinha deus não e que com a máquina ninguém não brinca porque ela mata. A máquina não era deus não, nem possuía os distintivos femininos de que o herói gostava tanto. Era feita pelos homens. Se mexia com eletricidade com fogo com água com vento com fumo, os homens aproveitando as forças da natureza. Porém jacaré acreditou? nem o herói! Se levantou na cama e com um gesto, esse sim! bem guaçu de desdém, tó! batendo o antebraço esquerdo dentro do outro dobrado, mexeu com energia a munheca direita pras três cunhas e partiu. Nesse instante, falam, ele inventou o gesto famanado de ofensa: a pacova.")
-    print(ass)
+    
+    ass1 = [4.33, 0.23, 0.14, 13.28, 1.86, 0.0] 
+    ass2 = [4.33, 0.23, 0.14, 13.28, 1.86, 0.0]
+
+    print(compara_assinatura(ass1, ass2))
