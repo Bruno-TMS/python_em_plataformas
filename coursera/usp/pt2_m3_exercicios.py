3403ddc37e7f63d61da10489f8be7b0a921da720
bruno silva <smtbssa@gmail.com>
2025-04-30 16:27:50 -0300

    atualizado método de calculo de triangulos

--- coursera/usp/pt2_m3_exercicios.py
+++ coursera/usp/pt2_m3_exercicios.py
@@ -4,40 +4,38 @@ Defina a classe Triangulo cujo construtor recebe 3 valores inteiros corresponden
 A classe triângulo também deve possuir um método perimetro, que não recebe parâmetros e devolve um valor inteiro correspondente ao perímetro do triângulo.
 """
 
+
 class Triangulo:
     def __init__(self, a, b, c):
-
+        
         if not self.validar_triangulo(a,b,c):
             raise ValueError('Triangulo Inválido')
-
+        
         self.a = a
         self.b = b
         self.c = c
-        self.perimetro = a + b + c
         
-        self.tipo_lado = None
         
-        if a == b == c:
-            self.tipo_lado = 'equilátero'
-        
-        elif a != b != c:
-            self.tipo_lado = 'escaleno'
-        
-        else:
-            self.tipo_lado = 'isósceles'
-
     def perimetro(self):
-        return self.perimetro
+        return sum([self.a, self.b, self.c])
     
     def tipo_lado(self):
-        return self.tipo_lado
+        count_lados_diferentes = len({l for l in [self.a, self.b, self.c]})
+        
+        if count_lados_diferentes == 3:
+            return 'escaleno'
+        
+        elif count_lados_diferentes == 2:
+            return 'isósceles'
+        
+        else:
+            return 'equilátero'
     
-
     @staticmethod
-    def validar_triangulo(a, b, c):
+    def _validar_triangulo(a, b, c):
         lados = [a,b,c]
         
-        if any([l<=0 for l in lados]):
+        if any([l <= 0 for l in lados]):
             return False
         
         if any([l > (sum(lados)-l) for l in lados]):
