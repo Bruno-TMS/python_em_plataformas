b0acbe2ac1648b07f6fba8d76781213747cfba11
bruno silva <smtbssa@gmail.com>
2025-05-01 12:17:47 -0300

    refatorado classe triangulo

--- coursera/usp/pt2_m3_exercicios.py
+++ coursera/usp/pt2_m3_exercicios.py
@@ -1,44 +1,64 @@
 """
 Exercício 1: Uma classe para triângulos
 Defina a classe Triangulo cujo construtor recebe 3 valores inteiros correspondentes aos lados a, b e c de um triângulo.
-A classe triângulo também deve possuir um método perimetro, que não recebe parâmetros e devolve um valor inteiro correspondente ao perímetro do triângulo.
+A classe triângulo também deve possuir um método perímetro, que não recebe parâmetros e devolve um valor inteiro correspondente ao perímetro do triângulo.
 """
-
 
 class Triangulo:
     def __init__(self, a, b, c):
         
-        if not self.validar_triangulo(a,b,c):
-            raise ValueError('Triangulo Inválido')
+        Triangulo.validar_triangulo(a, b, c)
         
-        self.a = a
-        self.b = b
-        self.c = c
+        self._a = a
+        self._b = b
+        self._c = c
         
-        
+    @property
+    def a(self):
+        return self._a
+    
+    @a.setter
+    def a(self,valor):
+        Triangulo.validar_triangulo(valor, self._b, self._c)
+        self._a = valor
+
+    @property
+    def b(self):
+        return self._b
+    
+    @b.setter
+    def b(self, valor):
+        Triangulo.validar_triangulo(self._a, valor, self._c)
+        self._b = valor
+    
+    @property
+    def c(self):
+        return self._c
+    
+    @c.setter
+    def c(self,valor):
+        Triangulo.validar_triangulo(self._a, self._b, valor)
+        self._c = valor
+    
     def perimetro(self):
         return sum([self.a, self.b, self.c])
     
     def tipo_lado(self):
-        count_lados_diferentes = len({l for l in [self.a, self.b, self.c]})
-        
-        if count_lados_diferentes == 3:
+        lados_unicos = len({l for l in [self.a, self.b, self.c]})
+    
+        if lados_unicos == 3:
             return 'escaleno'
         
-        elif count_lados_diferentes == 2:
+        elif lados_unicos == 2:
             return 'isósceles'
         
         else:
             return 'equilátero'
     
     @staticmethod
-    def _validar_triangulo(a, b, c):
+    def validar_triangulo(a, b, c):
         lados = [a,b,c]
         
-        if any([l <= 0 for l in lados]):
-            return False
-        
         if any([l > (sum(lados)-l) for l in lados]):
-            return False
-
-        return True
+            raise ValueError(f'Triangulo inválido para os lados: {a} {b} {c}')
+         
--- tests/test_pt2_m3_exercicios.py
+++ tests/test_pt2_m3_exercicios.py
@@ -1,13 +1,16 @@
+
+import pytest
 from  coursera.usp.pt2_m3_exercicios import Triangulo
 
-class TestTriangulo:
-       
-    def testa_validar_triangulo(self):
-        assert Triangulo.validar_triangulo(5,4,3) == True
-        assert Triangulo.validar_triangulo(0,0,0) == False
-        assert Triangulo.validar_triangulo(-1,0,0) == False
-        assert Triangulo.validar_triangulo(-1,0,2) == False
-        assert Triangulo.validar_triangulo(5,4,-3) == False
-        assert Triangulo.validar_triangulo(5,4,200) == False
+
+@pytest.mark.parametrize('a, b, c, is_triangulo',[(1, 1, 1, True),
+                                                  (5, 4, 3, True),
+                                                  (1, 1, -1, False),
+                                                  (0, 0, 0, False),
+                                                  (-5, -4, 3, False),
+                                                  (5, 4, 0, False),
+                                                  (5, 4, 200, False)])
+def test_validar_triangulo():
+    assert True
       
 
