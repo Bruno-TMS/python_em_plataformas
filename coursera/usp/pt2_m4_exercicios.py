ae9a80dbff35562612ea419669108309f73eb2a4
bruno silva <smtbssa@gmail.com>
2025-05-02 17:21:20 -0300

    criado metodo ordena

--- coursera/usp/pt2_m4_exercicios.py
+++ coursera/usp/pt2_m4_exercicios.py
@@ -1,6 +1,9 @@
+from random import randint
+
 def  ordenada(lista):
-    ordenada = sorted(lista)
-    return (lista[0] == ordenada[0]) and (lista[-1] == ordenada[-1])
+    pass
+    #ordenada = sorted(lista)
+    #return (lista[0] == ordenada[0]) and (lista[-1] == ordenada[-1])
 
 
 
@@ -11,3 +14,17 @@ def busca(lista, elemento):
     for i, e in enumerate(lista):
         if e == elemento:
             return i
+
+
+def lista_grande(n):
+    return [randint(0,10000) for r in range(n)]
+
+
+def ordena(lista:list):
+    ord_lst = []
+    while lista:
+        min_index = lista.index(min(lista))
+        ord_lst = lista.pop(min_index)
+    return ord_lst
+        
+
